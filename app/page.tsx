87cec1bc790397a657d8d9dfa0ae3bf1fa29f099
Dawid <dawidbugajski1996@gmail.com>
2023-07-23 11:29:50 +0200

    change main font for project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
-      <p>FEATURE - CONFIGURE TAILWIND</p>
+    <main className='flex flex-col items-center justify-center min-h-screen'>
+      <p className='text-2xl'>FEATURE - CONFIGURE TAILWIND</p>
+      <p className='text-2xl font-medium'>FEATURE - CONFIGURE TAILWIND</p>
+      <p className='text-2xl font-semibold'>FEATURE - CONFIGURE TAILWIND</p>
+      <p className='text-2xl font-bold'>FEATURE - CONFIGURE TAILWIND</p>
+      <p className='text-2xl font-extrabold'>FEATURE - CONFIGURE TAILWIND</p>
     </main>
   );
 }
