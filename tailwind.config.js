7bb38d7ca0a4f6288d0db721bd3b01addae09ba0
Dawid <dawidbugajski1996@gmail.com>
2023-07-31 10:00:08 +0200

    install shadcn  lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,21 @@ module.exports = {
       letterSpacing: {
         'heading-s': '2.4px',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
