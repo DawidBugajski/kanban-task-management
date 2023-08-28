01262e15beac8da063bf163c6404444b7fa1338f
Dawid <dawidbugajski1996@gmail.com>
2023-08-28 10:39:51 +0200

    truncate for boarditems

--- components/Sidebar/SidebarBoards.tsx
+++ components/Sidebar/SidebarBoards.tsx
@@ -56,24 +56,31 @@ const BoardItem = ({ board }: { board: Board }) => {
     <li
       onClick={handleSetActiveBoard}
       key={id}
-      className={`group transition-colors duration-100 cursor-pointer overflow-hidden flex items-center h-12 rounded-tr-[100px] rounded-br-[100px] 
+      className={`w-full group transition-colors duration-100 cursor-pointer overflow-hidden flex items-center h-12 rounded-tr-[100px] rounded-br-[100px] 
       ${isActiveBoard ? 'bg-purple text-white' : ''}
       ${!isActiveBoard && 'hover:bg-lightbg-hover-boards dark:hover:bg-white'}
       `}
     >
-      <span
-        className={`inline-flex items-center gap-4 ml-9 
-      ${!isActiveBoard && ' group-hover:text-purple'}
-      `}
-      >
-        <svg width='16' height='16' xmlns='http://www.w3.org/2000/svg'>
+      <div className='flex items-center w-full gap-4 overflow-hidden ml-9'>
+        <svg
+          className='shrink-0 min-w-16 min-h-16'
+          width='16'
+          height='16'
+          xmlns='http://www.w3.org/2000/svg'
+        >
           <path
             d='M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z'
             className='fill-current'
           />
         </svg>
-        {name}
-      </span>
+        <span
+          className={`truncate w-full ${
+            !isActiveBoard && ' group-hover:text-purple'
+          }`}
+        >
+          {name}
+        </span>
+      </div>
     </li>
   );
 };
