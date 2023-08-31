70f022d79c454a772bc4e9d0e9b316cd6d0f128f
Dawid <dawidbugajski1996@gmail.com>
2023-08-31 13:37:01 +0200

    refactore modal

--- components/shared/Modal.tsx
+++ components/shared/Modal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { useAppSelector } from '@/redux/hooks';
 import { useCloseModal } from '@/hooks/useCloseModal';
-import { currentModalContent, isOpenModal } from '@/redux/slices/modalSlice';
+import { isOpenModal } from '@/redux/slices/modalSlice';
 
 type DialogProps = {
   children: React.ReactNode;
@@ -10,16 +10,12 @@ type DialogProps = {
 
 export default function Modal({ children }: DialogProps) {
   const isOpen = useAppSelector(isOpenModal);
-  const isEditingColumnColor = useAppSelector(currentModalContent);
+
   const handleCloseModal = useCloseModal();
 
   return (
     <Dialog open={isOpen} onOpenChange={handleCloseModal}>
-      <DialogContent
-        className={`w-11/12 rounded-md dark:bg-dark-grey dark:text-white ${
-          isEditingColumnColor && 'overflow-hidden'
-        }`}
-      >
+      <DialogContent className='w-11/12 rounded-md dark:bg-dark-grey dark:text-white'>
         {children}
       </DialogContent>
     </Dialog>
